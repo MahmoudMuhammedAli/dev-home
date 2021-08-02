584b96c050df161cd5ccd4f768e50180c7484b20
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-08-02 20:38:55 +0200

    fetch the users api
    a user can sign in  if they have a profile or automatically create a new profile if not found,,
     also added a loading screen

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,43 +1,100 @@
-import React, {useState} from "react";
-import { useHistory } from "react-router-dom"
+import React, { useState, useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
-import {useAuth} from "../contexts/AuthContext"
+import { useAuth } from "../contexts/AuthContext";
 //styles
 import "../styles/css/chats.css";
+import axios from "axios";
 
 export default function Chats() {
-  const history = useHistory()
+  const history = useHistory();
   const [nav, setNav] = useState("Wanna Code?");
-  const {user} = useAuth();
+  const [loading, setLoading] = useState(true);
+  const { user } = useAuth();
 
-  function changeView(){
-    nav==="Wanna Code?"? 
-    setNav("back to chat"):setNav("Wanna Code?")
+  //get user's avatar
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    //use blob to transfer the image in binary formate
+    const date = await response.blob();
+
+    return new File([date], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+  console.log(user); 
+  useEffect(() => {
+    // prevent the user from accessing the chat with out login
+    if (!user) {
+      history.push("/");
+      return;
+    }
+    // fetch the users api
+    //signIn if you have chatEngine profile
+    axios
+      .get("https://api.chatengine.io/users/me/", {
+       
+        headers: {
+          "project-id": "013128ed-bcd2-484a-8d74-0ade7cb2c318",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => { 
+        setLoading(false);
+      })
+      //create a profile if you don't have one
+      .catch(() => {
+        let Data = new FormData();
+        Data.append("email", user.email);
+        Data.append("username", user.displayName);
+        Data.append("secret", user.uid);
+
+        getFile(user.photoURL).then((avatar) => {
+          Data.append("avatar", avatar, avatar.name);
+          axios.post("https://api.chatengine.io/users", Data, {
+            headers: { "private-key": "899a3cba-2a68-400c-b81e-051de9a65241" },
+          })
+          .then(() => setLoading(false))
+          .catch((error) => console.log(error))
+        });
+      });
+  }, [user, history]);
+
+  function changeView() {
+    nav === "Wanna Code?" ? setNav("back to chat") : setNav("Wanna Code?");
   }
   async function signOut() {
-    await auth.signOut()
-    history.push("/")
+    await auth.signOut();
+    history.push("/");
   }
 
+  //because there is no user once the page loads 
+  if(!user || loading)  
+  return (
+    <h1 className="loading"> loading...</h1>
+    )
+  
   return (
     <div className="chats_view">
       <div className="navbar">
         <div className="logo">Deve Home</div>
         <div className="links">
-          <div className="link code" onClick={changeView}>{nav}</div>
-          <div className="link signOut" onClick={signOut}>Sign out</div>
+          <div className="link code" onClick={changeView}>
+            {nav}
+          </div>
+          <div className="link signOut" onClick={signOut}>
+            Sign out
+          </div>
         </div>
       </div>
-      
+
       <ChatEngine
-        height= "calc(100vh - 66px)"
-        projectId="
+        height="calc(100vh - 66px)"
+        projectID="
         013128ed-bcd2-484a-8d74-0ade7cb2c318"
-        userName="."
-        userSecret="."
+        userName={user.email}
+        userSecret={user.uid}
       />
-      
     </div>
   );
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,28 +1,32 @@
-import React, { useContext, useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { auth } from "../firebase";
+  
+import React, { useContext, useState, useEffect } from "react"
 
-const AuthContext = React.createContext();
-export const useAuth = () => useContext(AuthContext);
+import { useHistory } from "react-router-dom"
+
+import { auth } from "../firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() { return useContext(AuthContext) }
 
 export function AuthProvider({ children }) {
-  const history = useHistory();
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState()
+  const history = useHistory()
 
   useEffect(() => {
     auth.onAuthStateChanged(user => {
-        setUser(user);
-        setLoading(false);
-         if(user){
-             history.push('/chats')
-            }
-    });
-  }, [user, history]);
-  const value = user;
-  return(
-      <AuthContext.Provider value={value}>
+      setUser(user)
+      setLoading(false)
+      if(user) history.push('/chats')
+    })
+  }, [user, history])
+
+  const value = { user }
+
+  return (
+    <AuthContext.Provider value={value}>
       {!loading && children}
-      </AuthContext.Provider>
+    </AuthContext.Provider>
   )
 }
