69d53c94529635ec2d43d9a7bba01cc763181b9d
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-08-02 17:00:51 +0200

    Add chats view
    awaiting setup

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import '../styles/css/chats.css'
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom"
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
+//styles
+import "../styles/css/chats.css";
 
 export default function Chats() {
-    return (
-      <div>
-      <h1>chats</h1>
+  const history = useHistory()
+  const [nav, setNav] = useState("Wanna Code?");
+
+  function changeView(){
+    nav==="Wanna Code?"? 
+    setNav("back to chat"):setNav("Wanna Code?")
+  }
+  async function signOut() {
+    await auth.signOut()
+    history.push("/")
+  }
+
+  return (
+    <div className="chats_view">
+      <div className="navbar">
+        <div className="logo">Deve Home</div>
+        <div className="links">
+          <div className="link code" onClick={changeView}>{nav}</div>
+          <div className="link signOut" onClick={signOut}>Sign out</div>
+        </div>
       </div>
-    )
+      <ChatEngine
+        height= "calc(100vh - 66px)"
+        projectId="
+        013128ed-bcd2-484a-8d74-0ade7cb2c318"
+        userName="."
+        userSecret="."
+      />
+      
+    </div>
+  );
 }
